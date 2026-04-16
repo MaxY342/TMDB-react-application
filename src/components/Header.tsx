ff1a5025ee922a70dd67af86d3f278e49c0b592a
MaxY342 <max.yang342@gmail.com>
2026-04-16 00:42:09 -0400

    update header and sidebar

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,14 @@
-import { Link } from '@/components/Link';
-import { SideBar } from '@/SideBar';
+import { Link } from '../components/Link';
+import { SideBar } from '../components/SideBar';
 
 export const Header = () => {
     return (
-        <SideBar />
-        <Link to="/">Home</Link>
-        <Link to="/movies">NowPlaying</Link>
-        <Link to="/tv">Search</Link>
-        <Link to="/trending">Trending</Link>
+        <div>
+            <SideBar />
+            <Link to="/">Home</Link>
+            <Link to="/movies">NowPlaying</Link>
+            <Link to="/tv">Search</Link>
+            <Link to="/trending">Trending</Link>
+        </div>
     );
 }
--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,14 +1,14 @@
-import { Link } from '@/components/Link';
+import { Link } from '../components/Link';
 
 export const SideBar = () => {
     return (
-        <div className="w-64 bg-gray-800 text-gray-300 h-screen p-4">
+        <div className="w-64 bg-gray-800 text-gray-300 h-screen p-4 group">
             <button>Menu</button>
-            <div className="hidden fixed top-0 h-screen w-[300px] bg-[#2f2f2f] shadow-[0px_8px_16px_0px_rgba(0,0,0,0.2)] z-[3] overflow-hidden whitespace-nowrap">
-                <Link className="text-white py-4 px-4 no-underline" to="/">Home</Link>
-                <Link className="text-white py-4 px-4 no-underline" to="/movies">NowPlaying</Link>
-                <Link className="text-white py-4 px-4 no-underline" to="/tv">Search</Link>
-                <Link className="text-white py-4 px-4 no-underline" to="/trending">Trending</Link>
+            <div className="hidden group:hover:block fixed top-0 h-screen w-[300px] bg-[#2f2f2f] shadow-[0px_8px_16px_0px_rgba(0,0,0,0.2)] z-[3] overflow-hidden whitespace-nowrap">
+                <Link to="/">Home</Link>
+                <Link to="/movies">NowPlaying</Link>
+                <Link to="/tv">Search</Link>
+                <Link to="/trending">Trending</Link>
             </div>
         </div>
     );
